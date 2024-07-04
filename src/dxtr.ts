91098101c6e8051ba740863641d32ec52d833287
astodialo <panteliskavv@gmail.com>
2024-07-04 13:39:21 +0200

    fee tx

--- src/dxtr.ts
+++ src/dxtr.ts
@@ -6,12 +6,37 @@ import {
     BaseWalletProvider,
     BlockfrostProvider,
     LucidProvider,
-    LiquidityPool
+    LiquidityPool,
+    DexTransaction,
 } from '@indigo-labs/dexter'
-
+import {
+    Blockfrost,
+    Lucid,
+} from 'lucid-cardano';
 import * as fs from 'fs';
 
 const seed = fs.readFileSync('./stuff/seed', 'utf8')
+const fee_addr = "";
+const raid_amt = 100n;
+const fee_amt = raid_amt * 2n / 10n
+const token = ""
+
+const lucid = await Lucid.new(
+    new Blockfrost(
+        "https://cardano-mainnet.blockfrost.io/api/v0",
+        "mainnetSilnqcY9CHmK7l2AzXAlX7HEaoLchT78"
+    ),
+    "Mainnet",
+);
+
+function createFeeTx(): DexTransaction {
+    const transaction: DexTransaction = new DexTransaction(lucidProvider);
+    transaction.providerData.tx = lucid
+        .newTx()
+        .payToAddress(fee_addr, { [token]: (fee_amt) });
+
+    return transaction;
+}
 
 const dexterConfig: DexterConfig = {
     shouldFetchMetadata: true,      // Whether to fetch asset metadata (Best to leave this `true` for accurate pool info)
@@ -34,11 +59,12 @@ const bfData: BaseDataProvider = new BlockfrostProvider(
     }
 );
 
-const lucidProvider: BaseWalletProvider = new LucidProvider();
+const lucidProvider: LucidProvider = new LucidProvider();
 lucidProvider.loadWalletFromSeedPhrase(seed.split(" "), {}, {
     url: 'https://cardano-mainnet.blockfrost.io/api/v0',
     projectId: 'mainnetSilnqcY9CHmK7l2AzXAlX7HEaoLchT78',
 })
+
 
 console.log(seed.split(" "))
 console.log(lucidProvider)
